d8549eb24d9e3ff2f3cd1027f79d15e6a68795be
EdCrossPi <eduardoxpimentel@gmail.com>
2025-04-21 19:31:59 -0300

    feat: update packages

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html", // Inclua o caminho para o seu HTML ou arquivos que utilizam Tailwind
+    "./src/**/*.{html,js}", // Inclua caminhos para qualquer arquivo JS ou HTML
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
