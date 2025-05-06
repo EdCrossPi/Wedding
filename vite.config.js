b4d34a0bf8038394c4dc327f9211d92179b48da9
EdCrossPi <eduardoxpimentel@gmail.com>
2025-05-05 23:52:02 -0300

    fix: vite and js script

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`,
+      },
+    },
   },
+  base: "./", // Mudança crucial para paths relativos
 });
-
-export default defineConfig({
-    base: '/',
-    build: {
-      outDir: 'dist',
-      assetsDir: 'assets'
-    }
-  })
